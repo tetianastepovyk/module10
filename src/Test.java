5e95d3a3dd2ae99313376f7c881c11046d9ef53b
Tetiana <mice14@ukr.net>
2023-01-23 13:44:37 +0200

    initial commit

---
+++ src/Test.java
@@ -0,0 +1,11 @@
+import task1.FileReadingValidPhoneNumbers;
+//import java.io.*;
+public class Test {
+    public static void main(String[] args) {
+        FileReadingValidPhoneNumbers fileReadingValidPhoneNumbers = new FileReadingValidPhoneNumbers();
+        fileReadingValidPhoneNumbers.printPhoneNumbersToConsole();
+    }
+
+
+
+}
