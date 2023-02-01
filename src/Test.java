4adbdbf76f37f8a5ef832fb7667924fe959bcc12
Tetiana <mice14@ukr.net>
2023-02-01 12:41:59 +0200

    initial commit

--- src/Test.java
+++ src/Test.java
@@ -1,9 +1,13 @@
 import task1.FileReadingValidPhoneNumbers;
+import task3.FrequencyOfWordInFile;
 //import java.io.*;
 public class Test {
     public static void main(String[] args) {
         FileReadingValidPhoneNumbers fileReadingValidPhoneNumbers = new FileReadingValidPhoneNumbers();
         fileReadingValidPhoneNumbers.printPhoneNumbersToConsole();
+
+        FrequencyOfWordInFile frequencyOfWordInFile = new FrequencyOfWordInFile();
+        frequencyOfWordInFile.printFrequencyOfWordInFileToConsole();
     }
 
 
